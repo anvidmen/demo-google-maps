ce8a750d6bead4a4860e2716a6638c29c2f275f7
Juan David Om <anvidmen@gmail.com>
2021-08-30 22:40:43 +0200

    add search component

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import {
+  Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption
+} from '@reach/combobox'
+import '@reach/combobox/styles.css'
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
 
-const SearchBox = () => {
-  return(
-    <div>
-      Search
+const SearchBox = ({ panTo }) => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 41.2257, lng: () => 2.1037 },
+      radius: 100 * 1000
+    }
+  })
+
+  const handleInput = (e) => setValue(e.target.value)
+
+  const handleSelect = async (address) => {
+    setValue(address, false)
+    clearSuggestions()
+
+    try {
+      const results = await getGeocode({ address })
+      const { lat, lng } = await getLatLng(results[0])
+      panTo({ lat, lng })
+    } catch (error) {
+      throw new Error('Error:', error)
+    }
+  }
+
+  return (
+    <div className='search'>
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder='Search places'
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === 'OK' &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
     </div>
   )
 }
